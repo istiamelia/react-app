cf11c6aafae2a94b9f38e74c9e20e1fdc91e3a34
istiamelia <istiamelia7@gmail.com>
2024-10-06 23:06:13 +0700

    First Commit
    First commit: 6 Oct 2024

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message";
+
+function App(){
+  // Use the Message component inside of the div
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+
+function Message(){
+    const name = "Nala"
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;
