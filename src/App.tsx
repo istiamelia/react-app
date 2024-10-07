f7625fd7f443d75c5441ce3ca5a157d2e756ddf2
istiamelia <istiamelia7@gmail.com>
2024-10-07 19:46:18 +0700

    Passing a function to the props, which is an even handler

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ function App() {
   // React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions.
   let items = ["New York", "San Fransisco", "Los Angeles", "Tokyo"];
   let heading = "Cities";
+  // start every name of an even handler with "handle"
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading={heading} />
+      <ListGroup
+        items={items}
+        heading={heading}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,8 +3,10 @@ interface Props {
   //  variable items has type of array string
   items: string[];
   heading: string;
+  //   Passing function via props
+  onSelectItem: (item: string) => void;
 }
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //   useState is a React Hook that lets you add a state variable to your component.
   //   The current state. During the first render, it will match the initialState you have passed.
   // The set function that lets you update the state to a different value and trigger a re-render.
@@ -24,7 +26,10 @@ function ListGroup({ items, heading }: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
