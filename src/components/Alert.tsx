346297d7f1493d6307aaeb03786f4c0fc0f2d251
istiamelia <istiamelia7@gmail.com>
2024-10-13 19:27:17 +0700

    Combine the Alert and Button component to create feature that toggles Alert Message through button onClick

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -5,16 +5,16 @@ interface Props {
   // if we rename the property to chilcren, we can pass the text inside of App Component like <Alert> "text" <Alert>
   children: ReactNode;
   //   ReactNode is a type that represents a React element, an array of React elements, or a string, number, or boolean
-  onClick: () => void;
+  onClose: () => void;
 }
 
-const Alert = ({ children, onClick }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
     <div className={"alert alert-warning alert-dismissible fade show "}>
       <strong>{children}</strong> You should check in on some of those fields
       below
       <button
-        onClick={onClick}
+        onClick={onClose}
         type="button"
         className="btn-close"
         data-bs-dismiss="alert"
